ad06f7a9480fc4c5311ddd933c65a1e9b089d962
Chung Phùng Phát <chphungphat@gmail.com>
2023-08-14 10:29:26 +0700

    Update styling for better display medias

--- src/pages/PersonalPage/Tab/MediaTab/index.jsx
+++ src/pages/PersonalPage/Tab/MediaTab/index.jsx
@@ -3,7 +3,10 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 
 function MediaTab() {
-    const [images, setImages] = useState([]);
+    const [images, setImages] = useState([{
+        id: '',
+        imageUrl: 'https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png'
+    }])
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/mediatab/1")
@@ -16,16 +19,25 @@ function MediaTab() {
     }, []);
 
     return (
-        <div className="card w-100 shadow-xss rounded-xxl border-0 mb-3 mt-3">
-            <div className="card-body d-block p-4" style={{ height: 10 }}>
+        <div className="card w-100 shadow-xss rounded-xxl border-0 mb-3 mt-3 ">
+            <div className="card-body d-block p-4" style={{height: 10}}>
                 <h4 className="fw-700 mb-3 font-xsss text-grey-900">Media List</h4>
             </div>
             <div className="card-body d-block pt-0 pb-2">
                 <div className="row">
-                    {images.map((image, index) => (
-                        <div className={index % 2 === 0 ? "col-6 mb-2 ps-1" : "col-6 mb-2 pe-1"} key={image.id}>
+                    {/*each row have 4 images*/}
+                    {images.map((image) => (
+                        <div className={'card col-3 p-1'} key={image.id}>
                             <Link to={`/media/${image.id}`} data-lightbox="roadtrip">
-                                <img src={image.imageUrl} alt="image" className="img-fluid rounded-3 w-100" />
+                                <div className='' style={{
+                                    width: '100%',
+                                    height: '200px',
+                                    backgroundImage: `url(${image.imageUrl})`,
+                                    backgroundSize: "cover",
+                                    backgroundPosition:"center",
+                                }}>
+
+                                </div>
                             </Link>
                         </div>
                     ))}
