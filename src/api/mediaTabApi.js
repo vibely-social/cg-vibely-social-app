c5fac9b4f9e0857e84987b6dd5678670a77961ac
Chung Phùng Phát <chphungphat@gmail.com>
2023-08-25 08:38:09 +0700

    Fix Api url, fix photos tab responsive UI

--- src/api/mediaTabApi.js
+++ src/api/mediaTabApi.js
@@ -5,7 +5,7 @@ export const getUserMedia = async () => {
     let result = null;
     let user = JSON.parse(localStorage.getItem('user'))
     try {
-        result = await axios.get(`${VIBELY_API}/posts/${user.id}`,{
+        result = await axios.get(`${VIBELY_API}/posts/user/${user.id}`,{
             headers:{
                 Authorization: 'Bearer ' + user.accessToken
             }
