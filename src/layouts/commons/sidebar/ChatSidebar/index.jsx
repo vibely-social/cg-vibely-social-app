83336c67f234cefea4f1879d5e5b3781e4f82c20
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-09-14 11:38:34 +0700

    refactor

--- src/layouts/commons/sidebar/ChatSidebar/index.jsx
+++ src/layouts/commons/sidebar/ChatSidebar/index.jsx
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 import {motion} from "framer-motion";
-import useViewport from "~/hooks/Viewport.jsx";
+import useViewport from "~/hooks/viewport.jsx";
 import {memo, useEffect, useState} from "react";
 import Container from 'react-bootstrap/Container';
 import {ListGroup} from "react-bootstrap";
