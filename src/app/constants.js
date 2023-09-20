dc9b9deef248e208d9bf41319be28e7b2ef1d487
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-09-20 09:30:58 +0700

    update gg clientId

--- src/app/constants.js
+++ src/app/constants.js
@@ -7,7 +7,7 @@ export const VIBELY_API = `https://${hostnameProduction}/api`
 
 export const PROVINCES_API = "https://provinces.open-api.vn/api/?depth=2"
 
-export const CLIENT_ID = "532640652631-tadeecfp8o33mipvo37n8fdp76vcq0i3.apps.googleusercontent.com"
+export const CLIENT_ID = "741684274930-o5e7d6f1r68ll85l4le05fisegmopil9.apps.googleusercontent.com"
 
 export const GOOGLE_USER_INFO_API = "https://www.googleapis.com/oauth2/v2/userinfo"
 
